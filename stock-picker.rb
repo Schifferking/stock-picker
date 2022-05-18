e66e2dd01a494341e6bd2be8a258adc06ace7dd3
Schifferking <schifferking@hotmail.com>
2022-05-18 12:44:59 -0500

    Create stock_picker function
    The stock_picker function receives an array (stock_prices) that
    represents a list of stock prices; it returns an array with the
    indices of the best days to buy and sell.
    
    To obtain that, first it creates an array to store all the possible
    profits based on the stock_prices, then it makes an array that
    calculates the profits. With those values now we can select the
    highest one and finally we can obtain the indices of the best days to
    buy and sell using the index method.

---
+++ stock-picker.rb
@@ -0,0 +1,21 @@
+def stock_picker(stock_prices)
+  profits = []
+  # Get the best day to sell for each day
+  stock_prices.each_with_index do |price, index|
+    if (index + 1) == stock_prices.length
+      break
+    end
+
+    sublist = stock_prices[(index + 1)..-1]
+    profits.push([price, sublist.max])
+  end
+
+  # Calculate the profits
+  profits_copy = profits.map {|profit| profit.last - profit.min }
+  highest_profit = profits_copy.max
+
+  hp_index = profits_copy.index(highest_profit)
+
+  # Return the indices of the best days to buy and sell
+  [stock_prices.index(profits[hp_index].first), stock_prices.index(profits[hp_index].last)]
+end
